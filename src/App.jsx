885d86265e5adcd9fafbc12740f670b1e573bfea
Austin Hartline <austinhartline@gmail.com>
2019-01-17 20:43:15 -0600

    hack-strapped with @salesforce/lightning-design-system

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from '@salesforce/design-system-react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Button label="weather?" variant="brand" />
+			</div>
+		);
+	}
+}
+
+export default App;
